a19f90f2b4e88c91dbb3e40e1634625c75719d72
Argueta <argueta03@hotmail.com>
2023-09-04 12:17:27 +0200

    hecho hasta marker

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYXJndWV0YTY5IiwiYSI6ImNsbGFrdXlsMjAzbHEzam05aXlvYnNtYmkifQ.S52KGmSJDcU8wrGTd4tMFg';
+
+
+if(!navigator.geolocation) {
+  alert('Navegador no soporta la Geolocation')
+  throw new Error('Navegador no soporta la Geolocation');
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
